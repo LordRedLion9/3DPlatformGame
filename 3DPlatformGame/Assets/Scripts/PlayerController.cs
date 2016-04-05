1136f0183a02b3410156ed074f364bbb469843b0
Alexander Ruston <rockyslammer@gmail.com>
2016-04-05 20:33:09 +1200

    Basic player movement
    Implemented basic player movement and basic platforms to move on for
    testing purposes

---
+++ 3DPlatformGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 7;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20F;
+    private float yVel = 0;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        CharacterController cc = GetComponent<CharacterController>();
+        Vector3 vel = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed;
+
+        if (cc.isGrounded)
+        {
+            yVel = 0;
+
+            if (Input.GetButton("Jump"))
+            {
+                yVel = jumpSpeed;
+            }
+
+        }
+
+        yVel -= gravity * Time.deltaTime;
+        vel.y = yVel;
+
+        cc.Move(vel * Time.deltaTime);
+
+    }
+
+
+}
+
