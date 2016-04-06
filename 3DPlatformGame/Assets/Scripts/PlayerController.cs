c920369de481902885dcc0549bf2da23d6da3729
Alexander Ruston <rockyslammer@gmail.com>
2016-04-06 21:42:41 +1200

    Added basic rolling
    Added a basic thrust on the shift key for the character as if it were
    doing a roll attack or a dodge, etc. To be refined later.

--- 3DPlatformGame/Assets/Scripts/PlayerController.cs
+++ 3DPlatformGame/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,14 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 10F;
-    public float jumpSpeed = 11.0F;
-    public float gravity = 20F;
+    public float jumpSpeed = 18.0F;
+    public float gravity = 40F;
     private float yVel = 0F; // Players current Y velocity
+
+    public float rollTimer = 0;
+    public float rollDistance = 15F;
+    public float rollTime = 0.5F;
+    public bool rolling = false;
 
     private Vector3 vel = Vector3.zero;
 
@@ -15,67 +20,98 @@ public class PlayerController : MonoBehaviour
     {
         CharacterController cc = GetComponent<CharacterController>();
 
-        //Horizontal Movement
-        if (Input.GetButton("Horizontal") && Mathf.Abs(vel.x) < speed)
+        if (rolling) //In Rolling state
         {
-            vel.x += Input.GetAxisRaw("Horizontal");
-        } else {
-            // friction stuff (TODO: Make neater)
-            if (vel.x < -0.1)
+            rollTimer -= Time.deltaTime;
+            
+            if (vel.x > 0.1) { vel.x -= 1; }
+            if (vel.x < -0.1) { vel.x += 1; }
+            if (vel.z > 0.1) { vel.z -= 1; }
+            if (vel.z < -0.1) { vel.z += 1; }
+            
+
+            if (rollTimer <= 0)
             {
-                vel.x++;
-            }
-            else if (vel.x > 0.1)
-            {
-                vel.x--;
-            }
-            else {
-                vel.x = 0;
+                rolling = false;
+                rollTimer = 0;
             }
             
         }
 
-        //Vertical Movement
-        if (Input.GetButton("Vertical") && Mathf.Abs(vel.z) < speed)
-        {
-            vel.z += Input.GetAxisRaw("Vertical");
-        }
-        else {
-            // friction stuff (TODO: Make neater)
-            if (vel.z < -0.1)
+        else { //In Normal state
+
+
+            //Horizontal Movement
+            if (Input.GetButton("Horizontal") && Mathf.Abs(vel.x) < speed)
             {
-                vel.z++;
-            }
-            else if (vel.z > 0.1)
-            {
-                vel.z--;
+                vel.x += Input.GetAxisRaw("Horizontal");
             }
             else {
-                vel.z = 0;
+                // friction stuff (TODO: Make neater)
+                if (vel.x < -0.1)
+                {
+                    vel.x++;
+                }
+                else if (vel.x > 0.1)
+                {
+                    vel.x--;
+                }
+                else {
+                    vel.x = 0;
+                }
+
             }
 
-        }
-
-        if (cc.isGrounded)
-        {
-            yVel = 0;
-
-            if (Input.GetButton("Jump")) //Jumping
+            //Vertical Movement
+            if (Input.GetButton("Vertical") && Mathf.Abs(vel.z) < speed)
             {
-                yVel = jumpSpeed;
+                vel.z += Input.GetAxisRaw("Vertical");
+            }
+            else {
+                // friction stuff (TODO: Make neater)
+                if (vel.z < -0.1)
+                {
+                    vel.z++;
+                }
+                else if (vel.z > 0.1)
+                {
+                    vel.z--;
+                }
+                else {
+                    vel.z = 0;
+                }
+
             }
 
-        }
+            if (cc.isGrounded)
+            {
+                yVel = 0;
 
+                if (Input.GetButton("Jump")) //Jumping
+                {
+                    yVel = jumpSpeed;
+                }
+
+                if (Input.GetButton("Fire3")) //rolling
+                {
+                    rolling = true;
+                    rollTimer = rollTime;
+                    vel = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * (rollDistance / rollTime);
+                }
+
+            }
+
+            
+            
+
+        }
         //Gravity
-        yVel -= gravity * Time.deltaTime; 
+        yVel -= gravity * Time.deltaTime;
         vel.y = yVel;
 
         //Add Movement
         cc.Move(vel * Time.deltaTime);
-
     }
-
 
 }
 
